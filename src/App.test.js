a79b2e21758e6c3b011943e5496b1a98b94b9b2e
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2021-12-01 22:59:18 -0300

    add operation button test

--- src/App.test.js
+++ src/App.test.js
@@ -2,22 +2,34 @@ import React from "react";
 import App from "./App";
 import { render, screen } from "@testing-library/react";
 
-describe("App", () => {
-  it("should show the bank name when the app is opened", () => {
-    render(<App />);
+describe("App component", () => {
+  describe("When the bank app is opened", () => {
+    it("should show the bank name when the app is opened", () => {
+      render(<App />);
 
-    const bankText = screen.getByRole("heading", {
-      name: /bytebank/i,
+      const bankText = screen.getByRole("heading", {
+        name: /bytebank/i,
+      });
+
+      expect(bankText).toBeInTheDocument();
     });
 
-    expect(bankText).toBeInTheDocument();
-  });
+    it("should show the account balance when the app is opened", () => {
+      render(<App />);
 
-  it("should show the account balance when the app is opened", () => {
-    render(<App />);
+      const balanceText = screen.getByText(/r\$ 1000/i);
 
-    const balanceText = screen.getByText(/r\$ 1000/i);
+      expect(balanceText).toBeInTheDocument();
+    });
 
-    expect(balanceText).toBeInTheDocument();
+    it("should show the create new operation when the app is loaded", () => {
+      render(<App />);
+
+      const newOperationButton = screen.getByRole("button", {
+        name: /realizar operação/i,
+      });
+
+      expect(newOperationButton).toBeInTheDocument();
+    });
   });
 });
